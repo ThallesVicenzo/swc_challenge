c2101df39a258d6c4bf6580757eb43d31549d056
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-14 15:32:37 -0300

    feat: add custom routes

---
+++ lib/view-model/routes/named_routes.dart
@@ -0,0 +1,5 @@
+class NamedRoutes {
+  static const splash = 'splash_screen';
+  static const grill = 'grill_rent_screen';
+  static const grillDetails = 'grill_rent_details_screen';
+}
---
+++ lib/view-model/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:swc_challenge/view/grill_rent/grill_rent_details_screen.dart';
+import 'package:swc_challenge/view/grill_rent/grill_rent_screen.dart';
+
+import '../../view/splash_screen.dart';
+import 'named_routes.dart';
+
+class Routes {
+  static Map<String, WidgetBuilder> all(BuildContext context) {
+    return {
+      NamedRoutes.splash: (context) => const Splash(),
+      NamedRoutes.grill: (context) => const GrillRent(),
+      NamedRoutes.grillDetails: (context) => const GrillRentDetails(),
+    };
+  }
+}
