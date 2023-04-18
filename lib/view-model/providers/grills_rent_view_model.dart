2e6044a206e3a823bbb468271cccbf8ac1f37a36
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-18 16:14:25 -0300

    feat: add new method to change implyLeading from appBar

--- lib/view-model/providers/grills_rent_view_model.dart
+++ lib/view-model/providers/grills_rent_view_model.dart
@@ -5,9 +5,15 @@ import '../../model/grills.dart';
 
 class GrillRentViewModel with ChangeNotifier {
   List<GrillsModel> grillsModel = [];
+  bool implyLeading = false;
 
   Future<void> returnData() async {
     final response = await GrillRepository.getGrillsList();
     grillsModel = response;
+  }
+
+  void changeLeading() {
+    implyLeading = true;
+    notifyListeners();
   }
 }
