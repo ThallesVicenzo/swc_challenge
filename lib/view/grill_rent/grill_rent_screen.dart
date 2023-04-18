23f6e9b245891bf29046d1e8f13a3ebeb2ea7e76
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-17 21:51:57 -0300

    feat: create GrillRentScreen

---
+++ lib/view/grill_rent/grill_rent_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:swc_challenge/view-model/providers/grills_rent_view_model.dart';
+import 'package:swc_challenge/view/grill_rent/widgets/grill_preview.dart';
+
+class GrillRent extends StatefulWidget {
+  const GrillRent({super.key});
+
+  @override
+  State<GrillRent> createState() => _GrillRentState();
+}
+
+class _GrillRentState extends State<GrillRent> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => GrillRentViewModel(),
+      child: Builder(
+        builder: (context) {
+          final viewModel = Provider.of<GrillRentViewModel>(context);
+          return Scaffold(
+            backgroundColor: Theme.of(context).primaryColor,
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).primaryColor,
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+              title: Text(
+                'SWC Challenge',
+                style: Theme.of(context).primaryTextTheme.bodyMedium,
+              ),
+            ),
+            body: FutureBuilder(
+              future: viewModel.returnData(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Theme.of(context).splashColor,
+                    ),
+                  );
+                }
+                return Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                          reverse: true,
+                          itemCount: viewModel.grillsModel.length,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10.0, vertical: 20.0),
+                          itemBuilder: (context, index) {
+                            return GrillPreview(
+                              grillsModel: viewModel.grillsModel[index],
+                            );
+                          }),
+                    ),
+                  ],
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
