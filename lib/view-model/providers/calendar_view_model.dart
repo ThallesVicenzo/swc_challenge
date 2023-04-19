2a75e53fd77e6d4be3b5e4eece3ce24d5689d823
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-19 11:29:19 -0300

    feat: add setData function and format timestamp to day/month

--- lib/view-model/providers/calendar_view_model.dart
+++ lib/view-model/providers/calendar_view_model.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:swc_challenge/view-model/repositories/firebase_repository.dart';
 
 class CalendarViewModel with ChangeNotifier {
+  int? _month;
+  int? _day;
+
+  static DateTime today = DateTime.now();
+  static String? formatedDate;
+  static DateTime lastDay = DateTime(5000);
+  static DateTime startDay = DateTime(2010);
+
   void updateDay(DateTime selectedDay, DateTime focusedDay) {
-    selectedDay = focusedDay;
-    focusedDay = selectedDay;
+    today = selectedDay;
+    _month = selectedDay.month;
+    _day = selectedDay.day;
+    formatedDate = '$_day/$_month';
     notifyListeners();
   }
 
-  void changeFormat() {}
+  Future<void> setData(String collection, String doc, String? date) async {
+    await FirebaseRepository.setDocumentCollectionData(collection, doc, date);
+    notifyListeners();
+  }
 }
