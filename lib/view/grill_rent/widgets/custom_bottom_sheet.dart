a646c181cc3de46a6f0a7327464eb214b4f90390
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-18 22:16:41 -0300

    feat: create CustomBottomSheet widget

---
+++ lib/view/grill_rent/widgets/custom_bottom_sheet.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:swc_challenge/model/grills.dart';
+
+import '../../../view-model/routes/named_routes.dart';
+
+class CustomBottomSheet extends StatelessWidget {
+  const CustomBottomSheet({super.key, required this.grillsModel});
+
+  final GrillsModel grillsModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).splashColor,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Image.asset(
+                grillsModel.image,
+                scale: 0.5,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Text(
+                    grillsModel.description.toString(),
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .primaryTextTheme
+                        .bodyMedium!
+                        .copyWith(color: Theme.of(context).primaryColor),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.95,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).highlightColor,
+                  width: 2,
+                ),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    NamedRoutes.calendar,
+                  );
+                },
+                child: Text(
+                  'Alugar',
+                  style: Theme.of(context).primaryTextTheme.bodyLarge,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/view/grill_rent/widgets/grill_preview.dart
+++ lib/view/grill_rent/widgets/grill_preview.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:swc_challenge/model/grills.dart';
-import 'package:swc_challenge/view-model/routes/named_routes.dart';
-import 'package:swc_challenge/view/calendar.dart';
+import 'package:swc_challenge/view/grill_rent/widgets/custom_bottom_sheet.dart';
 
 import '../../../view-model/providers/grills_rent_view_model.dart';
 
@@ -37,70 +36,7 @@ class GrillPreview extends StatelessWidget {
                 viewModel.changeLeading();
                 Scaffold.of(context).showBottomSheet<void>(
                   (BuildContext context) {
-                    return Container(
-                      color: Theme.of(context).splashColor,
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.asset(
-                                grillsModel.image,
-                                scale: 0.5,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    width: 2,
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(5.0),
-                                  child: Text(
-                                    grillsModel.description.toString(),
-                                    textAlign: TextAlign.center,
-                                    style: Theme.of(context)
-                                        .primaryTextTheme
-                                        .bodyMedium!
-                                        .copyWith(
-                                            color:
-                                                Theme.of(context).primaryColor),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.95,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Theme.of(context).highlightColor,
-                                  width: 2,
-                                ),
-                              ),
-                              child: TextButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(
-                                    context,
-                                    NamedRoutes.calendar,
-                                  );
-                                },
-                                child: Text(
-                                  'Alugar',
-                                  style: Theme.of(context)
-                                      .primaryTextTheme
-                                      .bodyLarge,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    );
+                    return CustomBottomSheet(grillsModel: grillsModel);
                   },
                 );
               },
