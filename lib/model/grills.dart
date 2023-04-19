dd5fa377ba30175fde700a07a6e98a63ed9f7190
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-04-19 11:28:10 -0300

    feat: add name data to model

--- lib/model/grills.dart
+++ lib/model/grills.dart
@@ -1,16 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GrillsModel {
+  final String name;
   final String image;
   final String? description;
 
   GrillsModel({
+    required this.name,
     required this.image,
     required this.description,
   });
 
   factory GrillsModel.fromJson(DocumentSnapshot<Map<String, dynamic>> map) {
     return GrillsModel(
+      name: map['name'].toString(),
       image: map['image'].toString(),
       description: map['descrição'].toString(),
     );
